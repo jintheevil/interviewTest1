9312419db6d7f2b423d869b26c5a609dc68e01d3
jintheevil <eugenelimjq1217@gmail.com>
2022-04-07 10:32:58 +0800

    added firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
+import { firebaseConfig } from './app.firebase.config';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    firebase.initializeApp(firebaseConfig)
+  }
 }
